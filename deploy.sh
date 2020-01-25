77ba92b9b7f4fe5307a5b8a1d45ecb2483243da6
celyes <celyes02@gmail.com>
2020-01-25 19:48:52 +0100

    update layout: default.twig

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,13 @@ cd output/
 
 mv ./blog/:year/:month/:day/index/index.html ./blog/index.html
 rm -rf blog/:year
+echo "[!] blog index's old directory: \e[31mDELETED\e[0m"
 git add .
 git commit -m "deploy"
 git push -f origin master
 
 cd ..
 
-echo "Deploy status:\e[32m OK \e[0m"
+echo "[+] Deploy status:\e[32m OK \e[0m"
 
 
