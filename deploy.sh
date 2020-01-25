ea5a31ed7b780cf49df9354582b63046e72262af
celyes <celyes02@gmail.com>
2020-01-25 19:38:20 +0100

    update structure

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 set -e
 
+cd /var/www/html/mysite/
+
 vendor/bin/statie generate source
 
 cd output/
 
-mv blog/:year/:month/:day/index/index.html ./blog/index.html
+mv ./blog/:year/:month/:day/index/index.html ./blog/index.html
 rm -rf blog/:year
 git add .
 git commit -m "deploy"
@@ -12,6 +14,6 @@ git push -f origin master
 
 cd ..
 
-echo "Deploy status:\e[31m OK \e[0m"
+echo "Deploy status:\e[32m OK \e[0m"
 
 
