1b6c760fcdf0ff9b30543c38bed831ab7af04c6d
celyes <celyes02@gmail.com>
2020-01-31 16:16:02 +0100

    update structure

--- source/assets/blog/js/main.js
+++ source/assets/blog/js/main.js
@@ -10,11 +10,10 @@ $(document).ready(function(){
         e.preventDefault();
         var val = $('#searchInput').val();
         if(val != ""){
-            var win = window.open('http://duckduckgo.com/'+val+' site:celyes.github.io', '_blank');
+            var win = window.open('http://google.com/search?q='+val+'+site:celyes.github.io', '_blank');
             win ? win.focus() : alert('Please allow popups for this website'); 
         }else{
             $('#searchInput').attr('class', 'is-invalid');
-            
         }
     });
 });
