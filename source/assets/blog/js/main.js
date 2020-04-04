583eeff8805fd3e67af0e81db45e2c10f0bc0468
celyes <celyes02@gmail.com>
2020-04-04 20:13:37 +0100

    update navbar

--- source/assets/blog/js/main.js
+++ source/assets/blog/js/main.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
     $('#searchInput').val("");
     $('.parallax').parallax();
     $('.button-collapse').sideNav();
+    $('.dropdown-button').dropdown({
+      inDuration: 300,
+      outDuration: 225,
+      constrainWidth: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: true, // Displays dropdown below the button
+      alignment: 'left', // Displays dropdown with edge aligned to the left of button
+      stopPropagation: false // Stops event propagation
+    });
     $('pre code').each(function(i, e){
         hljs.highlightBlock(e);
     });
