6c360f7541e421be0200662cb53956f7880d9c4b
celyes <celyes02@gmail.com>
2020-04-05 13:31:31 +0100

    minify js

--- source/assets/blog/js/comments.js
+++ source/assets/blog/js/comments.js
@@ -1,18 +1,1 @@
-var disqusThreadDiv = document.getElementById('disqus_thread');
-var commentsButton = document.getElementsByClassName('js-disqus-button')[0];
-
-//if (!disqusThreadDiv || !commentsButton) {return};
-
-var disqus_config = function () {
-  this.page.url = window.location.href; 
-  this.page.identifier = "{{post.id}}";
-};
-
-commentsButton.addEventListener('click', function(event) {
-  var d = document, s = d.createElement('script');
-  s.src = 'https://celyes-blog.disqus.com/embed.js'; 
-  s.setAttribute('data-timestamp', + new Date());
-  (d.head || d.body).appendChild(s);
-
-  event.target.parentElement.removeChild(event.target);
-});
+var disqusThreadDiv=document.getElementById("disqus_thread"),commentsButton=document.getElementsByClassName("js-disqus-button")[0],disqus_config=function(){this.page.url=window.location.href,this.page.identifier="{{post.id}}"};commentsButton.addEventListener("click",function(e){var t=document,s=t.createElement("script");s.src="https://celyes-blog.disqus.com/embed.js",s.setAttribute("data-timestamp",+new Date),(t.head||t.body).appendChild(s),e.target.parentElement.removeChild(e.target)});
