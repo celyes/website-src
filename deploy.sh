c18a1ec643a4dfa7400af48660efa83a93cbc7de
celyes <celyes02@gmail.com>
2020-04-05 12:31:19 +0100

    update disqus

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,10 @@ set -e
 
 if test -z "$1"
 then
-	echo "\e[31mProvide a commit message!\e[0m"
+	echo -e "\e[1;31mProvide a commit message!\e[0m"
 	exit
 fi
 vendor/bin/statie generate source
 git add .
 git commit -m "$1"
-echo "\e[32mDeployed successfully!\e[0m"
+echo -e "\e[1;32mDeployed successfully!\e[0m"
