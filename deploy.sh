46f5dab80cef72eda60f284e0dc69f002bff1cfc
celyes <celyes02@gmail.com>
2020-04-05 13:26:48 +0100

    add robots.txt file

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,10 @@ then
 	exit
 fi
 vendor/bin/statie generate source
+mv /var/www/html/website-src/output/404/index.html /var/www/html/website-src/output/404.html
+rm -rf /var/www/html/website-src/output/404/
+echo -e "\e[32mRelocated 404 page...\e[0m"
 git add .
 git commit -m "$1"
 git push origin master
-echo -e "\e[1;32mDeployed successfully!\e[0m"
+echo -e "\e[1;32mDeploy status: OK\e[0m"
