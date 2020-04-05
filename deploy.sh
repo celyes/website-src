d87ba1ba6c7d84393bfb22a44c40a9f19f5aaf1d
celyes <celyes02@gmail.com>
2020-04-05 12:11:09 +0100

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,12 @@
 #!/bin/bash 
 set -e
 
-if test -z $1 
+if test -z "$1"
 then
 	echo "\e[31;1mProvide a commit message!\e[0m"
 	exit
-else
-	vendor/bin/statie generate source
-	git add .
-	git commit -m "$1"
-	echo "\e[32;1mDeployed successfully!\e[0m"
 fi
+vendor/bin/statie generate source
+git add .
+git commit -m "$1"
+echo "\e[32;1mDeployed successfully!\e[0m"
