d4b90d5528a41de8123a5c279d1194dea1669b69
celyes <celyes02@gmail.com>
2020-04-05 15:06:58 +0100

    add regex logic

--- output/assets/blog/js/comments.js
+++ output/assets/blog/js/comments.js
@@ -1,15 +1,14 @@
 var pattern = /^\/blog\/([^?\/]+){2}\/([^?\/]+){2}\//gmi; 
 var p = window.location.pathname;
-
-console.log(p.match(pattern));
-
-var disqusThreadDiv = document.getElementById("disqus_thread"),
-    commentsButton = document.getElementsByClassName("js-disqus-button")[0],
-    disqus_config = function() {
-        this.page.url = window.location.href, this.page.identifier = "{{post.id}}"
-    };
-commentsButton.addEventListener("click", function(e) {
-    var t = document,
-        s = t.createElement("script");
-    s.src = "https://celyes-blog.disqus.com/embed.js", s.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(s), e.target.parentElement.removeChild(e.target)
-});
+if(p.match(pattern).indexOf(p) !== -1){
+	var disqusThreadDiv = document.getElementById("disqus_thread"),
+	    commentsButton = document.getElementsByClassName("js-disqus-button")[0],
+	    disqus_config = function() {
+	        this.page.url = window.location.href, this.page.identifier = "{{post.id}}"
+	    };
+	commentsButton.addEventListener("click", function(e) {
+	    var t = document,
+	        s = t.createElement("script");
+	    s.src = "https://celyes-blog.disqus.com/embed.js", s.setAttribute("data-timestamp", +new Date), (t.head || t.body).appendChild(s), e.target.parentElement.removeChild(e.target)
+	});	
+}
